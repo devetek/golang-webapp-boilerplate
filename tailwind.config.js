20c91b718bf8fc7d5b46579362faf5948bf6bb63
Prakasa <prakasa@devetek.com>
2024-07-14 07:29:53 +0700

    fix: fixing scripts nodeJS binary checking (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./internal/**/*.{html,js}", "./files/assets/**/*.{html,js}"],
+  content: ["./internal/**/*.{html,js}", "./files/assets/**/*.{css}"],
   theme: {
     extend: {},
   },
